fe096a0b14055c3ce4ab3d82727140af8b759bf2
Damm-Kwon <adamlukman0602@gmail.com>
2026-05-20 11:58:57 +0700

    part2 soal1

--- quiz/part2/soal1.js
+++ quiz/part2/soal1.js
@@ -1,10 +1,22 @@
-let word = 'JavaScript';
-let second = 'is';
-let third = 'awesome';
-let fourth = 'and';
-let fifth = 'I';
-let sixth = 'love';
-let seventh = 'it!';
-
-console.log(`${word}`,`${second}`,`${third}`,`${fourth}`,`${fifth}`,`${sixth}`,`${seventh}`)
-
+let word = "JavaScript";
+let second = "is";
+let third = "awesome";
+let fourth = "and";
+let fifth = "I";
+let sixth = "love";
+let seventh = "it!";
+console.log(
+  word +
+    " " +
+    second +
+    " " +
+    third +
+    " " +
+    fourth +
+    " " +
+    fifth +
+    " " +
+    sixth +
+    " " +
+    seventh,
+);
